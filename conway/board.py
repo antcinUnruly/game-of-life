81fcf7799130a5140976355e51046641c56a0feb
antcin <ciniglioantonio@gmail.com>
2018-10-21 20:30:43 +0100

    pass test_board_instantiation with minimum implementation

--- conway/board.py
+++ conway/board.py
@@ -4,7 +4,9 @@ class Board:
         self.alive_cells = []
 
     def make_alive_cells(self, cell_count):
-        for x in cell_count:
+        x = 0
+        while x < cell_count:
             self.alive_cells.extend([x])
+            x+=1
 
         # return cell_count
