d7cb8f4a9ba02b8fadde0f0d1342cdfd99c78049
antcinUnruly <antonio.ciniglio@unrulygroup.com>
2018-11-05 09:27:46 +0000

    write test to check if the neighbouring cells are correct

--- tests/test_board.py
+++ tests/test_board.py
@@ -21,3 +21,16 @@ class TestBoard():
         print(board.alive_cells)
 
         assert (len(board.find_neighbours_of_alive_cell(cell_1)) == 2)
+
+    def test_check_correct_neighbours_of_alive_cell(self):
+        board = Board()
+
+        cell_1 = Cell(Initial_status=True, x=5, y=5)
+        cell_2 = Cell(Initial_status=True, x=4, y=6)
+        cell_3 = Cell(Initial_status=True, x=5, y=4)
+
+        board.alive_cells = [cell_1, cell_2, cell_3]
+
+        assert(board.find_neighbours_of_alive_cell(cell_1) == [cell_2, cell_3])
+
+
