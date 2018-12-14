b7258f9c8501910a6bb082ac70a6ddcce99707fc
antcinUnruly <antonio.ciniglio@unrulygroup.com>
2018-12-14 13:04:41 +0000

    move find_neighbours_of_alive_cell to Game class

--- conway/game.py
+++ conway/game.py
@@ -18,27 +18,30 @@ class Game:
         # board = Board()
         for alive_cell in self.board.alive_cells:
             print('collecting neighbours of alive cell with coordinates', alive_cell.x, alive_cell.y)
-            alive_neighbours_number = len(self.board.find_neighbours_of_alive_cell(alive_cell))
+            alive_neighbours_number = len(self.find_neighbours_of_alive_cell(alive_cell))
 
             self.__run_logic(alive_cell, alive_neighbours_number)
         print('empty board', self.new_board.alive_cells)
         return self.new_board
 
-    def look_for_dead_cell(self):
-        dead_neighbours_of_alive_cell = []
+    def find_neighbours_of_alive_cell(self, cell):
+        print('Finding  neighbours of alive cell with coordinates', cell.x, cell.y)
+        x = cell.x
+        y = cell.y
+
+        x_minus_1 = x - 1
+        y_plus_1 = y + 1
+        x_plus_1 = x + 1
+        y_minus_1 = y - 1
+
+        neighbours_of_alive_cell = []
 
         for alive_cell in self.board.alive_cells:
-            # find locations where there is not alive cell
-            x = alive_cell.x
-            y = alive_cell.y
-
-            x_minus_1 = x - 1
-            y_plus_1 = y + 1
-            x_plus_1 = x + 1
-            y_minus_1 = y - 1
-
+            # ref code:
+            # http://blog.tplus1.com/blog/2007/06/24/using-dictionaries-rather-than-complex-if-elif-else-clauses/
             def push_cell_to_array():
-                dead_neighbours_of_alive_cell.append(alive_cell)
+                print('pushing', [(alive_cell.x, alive_cell.y)], 'to neighbours_of_alive_cell array')
+                neighbours_of_alive_cell.append(alive_cell)
 
             def do_nothing():
                 pass
@@ -60,23 +63,9 @@ class Game:
             dictionary_function = (combinations_dictionary[alive_cell_positions])
             dictionary_function()
 
-            # for key in combinations_dictionary.keys():
-            #     if key == (x, y):
-            #         pass
-            #     else:
-            #         dead_neighbours_of_alive_cell.append(key)
-
-
-        # print(dead_neighbours_of_alive_cell)
-        # cell_counter = Counter(dead_neighbours_of_alive_cell)
-        # for key, value in cell_counter.items():
-        #     if value == 3:
-        #         born_cell = Cell(x=key[0], y=key[1])
-        #         self.new_board.alive_cells.append(born_cell)
-        #         print(self.new_board.alive_cells)
+        return neighbours_of_alive_cell
 
     def __run_logic(self, cell, neighbours_number):
-        self.look_for_dead_cell()
         if cell in self.board.alive_cells:
             self.alive(cell, neighbours_number)
             return cell
@@ -94,4 +83,3 @@ class Game:
         if neighbours_number == 3:
             print('wooo')
             self.new_board.alive_cells.append(cell)
-            # print('dead cell array', self.board.alive_cells)
--- tests/test_game.py
+++ tests/test_game.py
@@ -5,9 +5,6 @@ from conway.cell import Cell
 from conway.board import Board
 
 
-# from conway.neighbours import Neighbours
-
-
 class TestGameOfLife():
 
     def test_fewer_than_two_live_neighbours(self):
@@ -39,25 +36,6 @@ class TestGameOfLife():
 
         assert (cell in new_board.alive_cells)
         assert (len(new_board.alive_cells) == 3)
-
-    # def test_two_live_neighbours_two(self):
-    #     cell = Cell(x=1, y=1)
-    #     neighbour1 = Cell(x=0, y=1)
-    #     neighbour2 = Cell(x=2, y=1)
-    #
-    #     cells_list = [cell, neighbour1, neighbour2]
-    #
-    #     board = Board(cells_list)
-    #
-    #     game = Game(board)
-    #
-    #     new_board = game.run()
-    #
-    #     # assert (cell in new_board.alive_cells)
-    #     for alive_cell in new_board.alive_cells:
-    #         print('cells, test',alive_cell.x, alive_cell.y)
-    #
-    #     assert (len(new_board.alive_cells) == 1)
 
     def test_three_live_neighbours(self):
         cell = Cell(x=1, y=1)
@@ -91,42 +69,4 @@ class TestGameOfLife():
         new_board = game.run()
 
         assert (cell not in new_board.alive_cells)
-
-    # def test_dead_cell_with_three_live_neighbours(self):
-    #     cell = Cell(x=1, y=1)
-    #
-    #     alive_cell_1 = Cell(x=0, y=1)
-    #     alive_cell_2 = Cell(x=1, y=2)
-    #     alive_cell_3 = Cell(x=2, y=1)
-    #
-    #     cells_list = [alive_cell_1, alive_cell_2, alive_cell_3]
-    #
-    #     board = Board(cells_list)
-    #
-    #     game = Game(board)
-    #
-    #     new_board = game.run()
-    #
-    #     assert (cell in new_board.alive_cells)
-
-    # def test_return_dead_neighbours(self):
-    #     cell = Cell(x=1, y=1)
-    #
-    #     alive_cell_1 = Cell(x=0, y=1)
-    #     alive_cell_2 = Cell(x=1, y=2)
-    #     alive_cell_3 = Cell(x=2, y=1)
-    #
-    #     cells_list = [alive_cell_1, alive_cell_2, alive_cell_3]
-    #
-    #     board = Board(cells_list)
-    #
-    #     game = Game(board)
-    #
-    #     game.run()
-    #
-    #     #https://stackoverflow.com/questions/16013485/counting-the-amount-of-occurrences-in-a-list-of-tuples
-    #
-    #     assert (game.look_for_dead_cell(cell) == [cell_2, cell_3])
-
-
 
