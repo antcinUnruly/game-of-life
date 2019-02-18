9afc72050530248254b540047e2a8fca97b6f3ad
antcin <ciniglioantonio@gmail.com>
2019-02-18 22:14:00 +0000

    print out stuff in main function;

--- conway/experiment_runner.py
+++ conway/experiment_runner.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 class ExperimentRunner:
     def __init__(self):
-        self.game = None
+        self.game = Game(None)
 
         # print('board', game.board.alive_cells)
         # for x in game.board.alive_cells:
@@ -12,22 +12,30 @@ class ExperimentRunner:
         # # print('new board', game.run().alive_cells)
 
     def run_experiment(self):
-        self.game = Game(None)
-        for x in self.game.board.alive_cells:
-            print(x.x, x.y)
+
+        # for x in self.game.board.alive_cells:
+        #     print(x.x, x.y)
         #
+        # #
+        # new_board = self.game.run()
+        # new_board2 = self.game.run()
+
         # print('hi', self.game.board.alive_cells)
-        return self.game.run()
+        # return new_board
+        return self.game.board
 
 
 
 def main():
+        i = 0
+        while i < 10:
+            experiment = ExperimentRunner()
 
-        experiment = ExperimentRunner()
+            result = experiment.run_experiment()
+            i +=1
 
-        result = experiment.run_experiment()
-
-        print(result)
+            for x in result.alive_cells:
+                print('cell in ', result, x.x, x.y)
 
 
 main()
